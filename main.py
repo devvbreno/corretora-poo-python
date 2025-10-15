78c3390fe1f813ff46c01dd2a3ed60a48e6b4b16
devvbreno <brenoelias05@gmail.com>
2025-10-15 17:28:18 -0300

    Feat: Aplica encapsulamento com getters e setters na classe ativo

---
+++ main.py
@@ -0,0 +1,8 @@
+from corretora_app.models.ativo import Ativo
+
+ativo1 = Ativo(ticker = "PETR4", nome_empresa = "Petrobrás S.A", preco_atual = 35.20)
+ativo2 = Ativo(ticker = "VALE3", nome_empresa = "Vale S.A", preco_atual = 62.80)
+
+
+ativo1 = Ativo("")
+print (ativo1)
